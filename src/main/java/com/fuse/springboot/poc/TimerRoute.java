344ca7fe6254461a37a80a3ab4a24833229a02bc
Soman <579713@cognizant.com>
2018-06-07 16:58:06 +0530

    Initial draft version

---
+++ src/main/java/com/fuse/springboot/poc/TimerRoute.java
@@ -0,0 +1,18 @@
+package com.fuse.springboot.poc;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class TimerRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+	from("timer://foo?fixedRate=true&period=60000")
+	.log("'Timer Triggered ##########");
+	
+	}
+	
+	
+
+}
