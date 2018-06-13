cb49670545ab890f7a503ba3b8b3d363cb94a506
Soman <579713@cognizant.com>
2018-06-13 11:56:28 +0530

    Updated

--- src/main/java/com/fuse/springboot/poc/TimerRoute.java
+++ src/main/java/com/fuse/springboot/poc/TimerRoute.java
@@ -6,13 +6,20 @@ public class TimerRoute extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		
+		// TODO Auto-generated method stub
 		
 	from("timer://foo?fixedRate=true&period=60000")
 	.log("'Timer Triggered ##########");
 	
 	}
 
+
+private void display()
+{
+
+System.out.println("Show me");
+
+}
 	
 	
 
